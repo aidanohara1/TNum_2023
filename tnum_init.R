b4add9f60221e7098284d81fac0c9b9c14cfcf66
aidanohara1 <aohara1@bu.edu>
2023-07-07 19:12:50 -0400

    redraft

--- tnum_init.R
+++ tnum_init.R
@@ -7,16 +7,49 @@ install_github("truenumbers/tnum/tnum")
 library(tnum)
 tnum.loadLibs()
 tnum.authorize()
+# numberspace: http://dev.truenumbers.com/query
+# API: http://dev.truenumbers.com:8083/
 tnum.getSpace()
 tnum.setSpace("TN-BIZDEV1")
 
-tnum.query()
+#tnum.createSpace("yourSpaceNameHere")
 
+#tnum.query()
+
+# example templates
 templates <- list(
   c("$(city) $(state) has population = $(population)", "state:$str_to_upper(state),ingest:$dateTkn()"),
   c("$(city) $(state) has mean temperature = $(meanTempF) F", "state:$(state)")
 )
 
+
+# dont actually know how to format this template list
+# tnum.ingestDataFrame(mtcars, list())
+
+
+library(ISLR)
+View(Auto)
+# remove special charecters from name string
+Auto2 <- dplyr::mutate(Auto,name2 = gsub("[()@']", "",name))
+
+# ingest of numbers from Auto 2
+
+car_templates <- list(c("$(name2) has fuel economy = $(mpg) mpg","test-7"))
+# need from allen, the shorthand list of units
+tnum.ingestDataFrame(df = Auto2,templates = car_templates)
+# success
+
+car_templates2 <- list(c("$(name2) has mass = $(weight) lb","test-6"))
+
+tnum.ingestDataFrame(df = Auto2, templates = car_templates2)
+
+car_templatres_All <- list(c("$(name2) has mass = $(weight) lb","test-6"),
+                           c("$(name2) has fuel economy = $(mpg) mpg","test-7"))
+
+# examples for deletion by query
+#tnum.deleteByQuery(query = "* has weight")
+
+# OTHER NOTES #
 
 # What customers and market could this software address?
 
@@ -30,30 +63,8 @@ templates <- list(
 # A) driven to empower our users to use tnum themselves?
 # B) empowered to drive tnum for users benefit?
 
-
 # 25k a year, 12 years ago, for a small # of execs
 
 # two cases that have been certainly addressed:
 # A) all your data is in excel and that's bad
 # B) all your data is all over the place and you don't know how to line it up
-
-
-View(mtcars)
-# dont actually know how to format this template list
-# tnum.ingestDataFrame(mtcars, list())
-
-
-library(ISLR)
-View(Auto)
-
-car_temps <- list(c("$(name2) has fuel economy = $(mpg) mpg","test-7"))
-
-tnum.ingestDataFrame(df = Auto2,templates = car_temps)
-# success
-
-Auto2 <- dplyr::mutate(Auto,name2 = gsub("[()@']", "",name))
-
-car_temp2 <- list(c("$(name2) has mass = $(weight) lb","test-6"))
-tnum.ingestDataFrame(df = Auto2, templates = car_temp2)
-
-tnum.deleteByQuery(query = "* has weight")
